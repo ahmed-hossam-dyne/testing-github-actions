d35f4b4795c020f720c1af23679ba5fd3a88bd90
ahmed-hossam-dyne <ahmed.hossam@dyne-app.com>
2023-08-12 17:04:57 +0300

    updated environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB7N0JCGu2AT_xXkJAcux8R-NQIG8GTVVg",
+    authDomain: "github-actions-ef385.firebaseapp.com",
+    projectId: "github-actions-ef385",
+    storageBucket: "github-actions-ef385.appspot.com",
+    messagingSenderId: "651953692895",
+    appId: "1:651953692895:web:79012ca6d3a6bbc41d896a"
+  }
 };
